5fd6293cb0e97f97e00689111708e2e60fdf6f11
Arin <abrandsson@gmail.com>
2015-08-24 16:19:36 +0200

    Added ipython

---
+++ Øvelser/ipython_notebook_config.py
@@ -0,0 +1,13 @@
+# Password to use for web authentication
+c = get_config()
+
+# Kernel config
+c.IPKernelApp.pylab = 'inline'  # if you want plotting support always
+
+# Notebook config
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = True
+c.NotebookApp.password = u'sha1:309896beb51d:fdbec23ebe5710eae1efbd99b5b0dc89fa36c507'
+# It is a good idea to put it on a known, fixed port
+c.NotebookApp.port = 1337
+
